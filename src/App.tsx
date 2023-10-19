405964a6230eaf5bccff42677b61520b37bcb39d
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2023-10-19 11:49:37 +0200

    setLap

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
 import { useCount } from "./hooks/useCount";
+import { useEffect } from "react";
 
 const App = () => {
-  /* Controller - Importo le funzioni che mi servono  */
+
+  /* Controller - Importo le funzioni che mi servono da "useCount.ts" */
   const { count, add, remove, reset, moltiplicatore } = useCount();
+  const [lap, setLap] = useState(0);
+
+  /* Stampa valore count + lap */
+  useEffect (() => {
+    if(count % 2 === 0){
+      setLap(lap + 1);
+    }
+  }, [count])
 
   return (
     /* View */
     <div>
       <h3>{count}</h3>
+      <h4>Lap: {lap}</h4>
       <button onClick={add}>+</button>
       <button onClick={remove}>-</button>
       <button onClick={reset}>Reset</button>
